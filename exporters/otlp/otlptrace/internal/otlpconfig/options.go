abf91b7b8f0177df44310a2a85da2523555a84e1
adevjoe <adevjoe@gmail.com>
2023-03-03 16:07:20 +0800

    fix trace

--- exporters/otlp/otlptrace/internal/otlpconfig/options.go
+++ exporters/otlp/otlptrace/internal/otlpconfig/options.go
@@ -22,7 +22,6 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/backoff"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/encoding/gzip"
 
 	"go.opentelemetry.io/otel/exporters/otlp/internal"
@@ -110,7 +109,7 @@ func NewGRPCConfig(opts ...GRPCOption) Config {
 	if cfg.Traces.GRPCCredentials != nil {
 		cfg.DialOptions = append(cfg.DialOptions, grpc.WithTransportCredentials(cfg.Traces.GRPCCredentials))
 	} else if cfg.Traces.Insecure {
-		cfg.DialOptions = append(cfg.DialOptions, grpc.WithTransportCredentials(insecure.NewCredentials()))
+		cfg.DialOptions = append(cfg.DialOptions, grpc.WithTransportCredentials(NewCredentials()))
 	} else {
 		// Default to using the host's root CA.
 		creds := credentials.NewTLS(nil)
